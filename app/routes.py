ea23bf1bcb9ebfab5c403fb1d686fb35d0c361f3
francescafr <hello@francesca.fyi>
2022-11-01 09:51:36 -0700

    added external database

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
+
 # class Book:
 #     def __init__(self, id, title, description):
 #         self.id = id
@@ -11,12 +14,38 @@ from flask import Blueprint, jsonify, abort, make_response
 #     Book(3, "The Great Train Robbery", "Historical Fiction novel"),
 # ]
 
-#books_bp = Blueprint("books", __name__, url_prefix="/books")
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET","POST"])
+def handle_books():
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id" : book.id,
+                "title" : book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST": 
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"], description=request_body["description"])
+        
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} successfully created", 201)
+
+# @books_bp.route("/<id>", methods=["GET"])
+# def handle_book():
+#     if request.method == "GET":
+#         book = Book.query.get
 
 # @books_bp.route("", methods=["GET"])
 # def handle_books():
 #     book_list = []
-#     for book in books:
+#     for book in book_list:
 #         book_list.append({
 #             "id" : book.id, 
 #             "title" : book.title, 
